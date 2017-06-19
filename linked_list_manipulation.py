fe4e9113fdd0d7eb3571512e5f2b73a2c8685163
Rosswell <blanchard.ross@gmail.com>
2017-06-19 16:50:15 -0700

    final linked list cycle/duplicate edge breaking

--- linked_list_manipulation.py
+++ linked_list_manipulation.py
@@ -6,14 +6,47 @@ You can use helper methods .get(index) and .set(index, (new_value)) to get and s
 '''
 
 '''Explanation:
-Given the constraints, there are basically two cases that we want to control for: cycles and multiple links.
-Cycles will be in the following format: [(n1, n2),...(nx, n1)]
-In this case, we want to:
-1. check to see if there is a link after the loop occurs
-2. if no, set the loop edge pointer to Null
-3. if yes, skip the node causing the loop
-Multiple links will be in the following format: [(n1, n2),...(n1, nx)]
-In this case, we want to:
-1. check to see if there is a link after the multiple links occur
-2. if yes, delete the link and 
+Given the constraints, there are basically two cases that we want to control for: cycles and multiple links. Will be
+maintaining 2 lists in addition to the original: one to store previously seen values, one to return.
+1. Iterate through the list of edges, check if the source node (n1 in (n1, n2)) is already in the seen nodes dict
+2. If it's not in the seen nodes dict, add it to the dict and make sure n1's pointer doesn't create a cycle or is the
+second edge from that node. If those are both true, append the edge to the return list
+3. If it is in the dict, check if there is a cycle by comparing to the previous edge. If a cycle is present, append an edge
+containing (n1, n4), where original two edges were [(n1, n2)(n3, n4)], effectively skipping the node creating the cycle
+4. All other cases are skipped, as they are not the original edges from a particular source node
 '''
+
+from operator import itemgetter
+
+class linked_list(object):
+    def __init__(self, edge_list):
+        self.edge_list = edge_list
+
+    def get(self, index):
+        return self.edge_list[index]
+
+    def set(self, index, new_value):
+        self.edge_list[index] = new_value
+        return self.edge_list
+
+    def list_iter(self):
+        ret_list = []
+        seen_dict = {}
+        for i, edge in enumerate(self.edge_list):
+            node_from, node_to = itemgetter(0, 1)(edge)
+            if node_from not in seen_dict:
+                # new node addition to seen dict
+                seen_dict[node_from] = True
+                if node_to not in seen_dict:
+                    # source and destination nodes are unique and create no cycles
+                    ret_list.append(edge)
+            else:
+                prev_node_from, prev_node_to = itemgetter(0, 1)(self.edge_list[i-1])
+                if prev_node_to == node_from:
+                    # cycling case - skips the cycled node to preserve path continuity
+                    ret_list.append((prev_node_from, node_to))
+        return sorted(list(set(ret_list)))
+
+input_list = [('n1', 'n2'), ('n2', 'n3'), ('n3', 'n1'), ('n1', 'n4'), ('n4', 'n5'), ('n1', 'n123')]
+x = linked_list(input_list)
+print(x.list_iter())
